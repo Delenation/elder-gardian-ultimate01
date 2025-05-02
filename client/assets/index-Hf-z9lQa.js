cfb559328331a9a9c91bbc05fa59c8ade64a545a
Delenation <Illidanthunderism@foxmail.com>
2025-05-02 14:29:39 +0800

    Updates

---
+++ client/assets/index-Hf-z9lQa.js
@@ -0,0 +1,1 @@
+import{i as a}from"./index-D-rFwgxL.js";import"./with-props-DZhwF4zh.js";import"./chunk-BAXFHI7N-ZcDp_2da.js";import"./index-CmoDiCTr.js";import"./index-CNHauDWF.js";export{a as default};
---
+++ client/assets/redirectIndex-DivcnRbl.js
@@ -0,0 +1,1 @@
+import{w as t}from"./with-props-DZhwF4zh.js";import{j as e,N as r}from"./chunk-BAXFHI7N-ZcDp_2da.js";const n=t(function(){return e.jsx(r,{to:"/dashboard",replace:!0})});export{n as default};
